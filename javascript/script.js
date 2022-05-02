92e62d9950d6b7a5994b04b4674d66d8ed411c0d
Ennio-M <marrese.ennio@gmail.com>
2022-05-03 01:48:44 +0200

    bonus

--- javascript/script.js
+++ javascript/script.js
@@ -2,18 +2,25 @@ const app = new Vue({
     el: '#app',
     data: {
         numEmails: 10,
-        emailList: []
+        emailList: [],
+        ready: null
     },
     methods: {
-
+        reset() {
+            this.ready = true;
+            this.emailList = [];
+            for(let i = 0; i < this.numEmails; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+                    this.emailList.push(res.data.response)
+                }).catch((error) => {
+                    console.log(error)
+                }).finally(() => {
+                    this.emailList.length === this.numEmails ? this.ready= false : '';
+                })
+            }
+        }
     },
     mounted(){
-        for(let i = 0; i < this.numEmails; i++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
-                this.emailList.push(res.data.response)
-            }).catch((error) => {
-                console.log(error)
-            })
-        }
+        this.reset();
     }
 })
